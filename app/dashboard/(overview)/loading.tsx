af6c0ac7fedc43af6b9b6104f93f474f4b38dcf9
shantoKonaSl <shanto@konasl.com>
2023-11-17 13:25:46 +0600

    Invoices page done

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,11 @@
+/**
+ * @Author: Jahidul Haque Shanto
+ * @Date:   2023-11-15 10:52:43
+ * @Last Modified by:   Jahidul Haque Shanto
+ * @Last Modified time: 2023-11-15 12:27:09
+ */
+import DashboardSkeleton from '@/app/ui/skeletons'
+
+export default function Loading() {
+  return <DashboardSkeleton />
+}
